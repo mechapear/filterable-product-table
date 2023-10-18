7d3ab17057d0ee1e01e6b0145f2f5f9371cb2a97
mechapear <mechapear@gmail.com>
2023-10-18 14:53:32 +0700

    feat: add a static version app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,50 @@
 import './App.css'
+import { Products, PRODUCTS_DATA } from './domain/products.ts'
+
+function productRows(PRODUCTS_DATA: Products[]) {
+  const rows = []
+  let prevCategory = ''
+
+  for (let i = 0; i < PRODUCTS_DATA.length; i++) {
+    // Add category row if it's a new category
+    if (PRODUCTS_DATA[i].category !== prevCategory) {
+      rows.push(
+        <tr>
+          <th>{PRODUCTS_DATA[i].category}</th>
+        </tr>,
+      )
+    }
+    // Add product row
+    rows.push(
+      <tr>
+        <td>{PRODUCTS_DATA[i].name}</td>
+        <td>{PRODUCTS_DATA[i].price}</td>
+      </tr>,
+    )
+    prevCategory = PRODUCTS_DATA[i].category
+  }
+  return rows
+}
 
 export default function App() {
-  return <></>
+  return (
+    <>
+      <form className="searchBar">
+        <input type="text" placeholder="Search" />
+        <input type="checkbox" />
+        <label>Only show products in stock</label>
+      </form>
+      <div className="productTable">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>{productRows(PRODUCTS_DATA)}</tbody>
+        </table>
+      </div>
+    </>
+  )
 }
