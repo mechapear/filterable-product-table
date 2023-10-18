1f79be1bf1e545a4df1f41c92ce8402297ec095d
mechapear <mechapear@gmail.com>
2023-10-18 17:51:04 +0700

    feat: extract ProductTable into a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,6 @@
 import './App.css'
-import { Products, PRODUCTS_DATA } from './domain/products.ts'
-import SearchBar from './SearchBar.tsx'
-
-function productRows(PRODUCTS_DATA: Products[]) {
-  const rows = []
-  let prevCategory = ''
-
-  for (let i = 0; i < PRODUCTS_DATA.length; i++) {
-    // Add category row if it's a new category
-    if (PRODUCTS_DATA[i].category !== prevCategory) {
-      rows.push(
-        <tr>
-          <th className="categoryRow" colSpan={2}>
-            {PRODUCTS_DATA[i].category}
-          </th>
-        </tr>,
-      )
-    }
-    // Add product row
-    rows.push(
-      <tr>
-        <td className="productRow">{PRODUCTS_DATA[i].name}</td>
-        <td className="productRow">{PRODUCTS_DATA[i].price}</td>
-      </tr>,
-    )
-    prevCategory = PRODUCTS_DATA[i].category
-  }
-  return rows
-}
+import ProductTable from './components/ProductTable'
+import SearchBar from './components/SearchBar'
 
 export default function App() {
   return (
@@ -35,15 +8,7 @@ export default function App() {
       <div className="boardWrapper">
         <SearchBar />
         <div className="tableWrapper">
-          <table className="productTable">
-            <thead>
-              <tr>
-                <th className="headerRow">Name</th>
-                <th className="headerRow">Price</th>
-              </tr>
-            </thead>
-            <tbody>{productRows(PRODUCTS_DATA)}</tbody>
-          </table>
+          <ProductTable />
         </div>
       </div>
     </>
---
+++ src/components/ProductTable/ProductTable.tsx
@@ -0,0 +1,42 @@
+import { Products, PRODUCTS_DATA } from '../../domain/products.ts'
+
+function productRows(PRODUCTS_DATA: Products[]) {
+  const rows = []
+  let prevCategory = ''
+
+  for (let i = 0; i < PRODUCTS_DATA.length; i++) {
+    // Add category row if it's a new category
+    if (PRODUCTS_DATA[i].category !== prevCategory) {
+      rows.push(
+        <tr>
+          <th className="categoryRow" colSpan={2}>
+            {PRODUCTS_DATA[i].category}
+          </th>
+        </tr>,
+      )
+    }
+    // Add product row
+    rows.push(
+      <tr>
+        <td className="productRow">{PRODUCTS_DATA[i].name}</td>
+        <td className="productRow">{PRODUCTS_DATA[i].price}</td>
+      </tr>,
+    )
+    prevCategory = PRODUCTS_DATA[i].category
+  }
+  return rows
+}
+
+export default function ProductTable() {
+  return (
+    <table className="productTable">
+      <thead>
+        <tr>
+          <th className="headerRow">Name</th>
+          <th className="headerRow">Price</th>
+        </tr>
+      </thead>
+      <tbody>{productRows(PRODUCTS_DATA)}</tbody>
+    </table>
+  )
+}
