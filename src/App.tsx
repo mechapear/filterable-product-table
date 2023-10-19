7c1ca34f8ab018654213d4e276c855089d9debd0
mechapear <mechapear@gmail.com>
2023-10-19 14:33:51 +0700

    feat: add useState to keep track SearchBar's input

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import './App.css'
+import { useState } from 'react'
 import ProductTable from './components/ProductTable'
 import SearchBar from './components/SearchBar'
 
 export default function App() {
+  const [searchText, setSearchText] = useState('')
+  const [queryInStock, setQueryInStock] = useState(false)
+
   return (
     <>
       <div className="boardWrapper">
-        <SearchBar />
+        <SearchBar
+          searchBoxValue={searchText}
+          onSearchTextChange={setSearchText}
+          checkboxValue={queryInStock}
+          onCheckboxChange={setQueryInStock}
+        />
         <div className="tableWrapper">
           <ProductTable />
         </div>
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,14 +1,33 @@
-export default function SearchBar() {
+export type SearchBarProps = {
+  searchBoxValue: string
+  onSearchTextChange: (value: string) => void
+  checkboxValue: boolean
+  onCheckboxChange: (value: boolean) => void
+}
+
+export default function SearchBar({
+  searchBoxValue,
+  onSearchTextChange,
+  checkboxValue,
+  onCheckboxChange,
+}: SearchBarProps) {
   return (
     <form className="searchBar">
       <input
         className="searchBox"
         type="text"
         placeholder="Search products..."
+        value={searchBoxValue}
+        onChange={(event) => onSearchTextChange(event.target.value)}
       />
       <label className="searchOption">
         {' '}
-        <input type="checkbox" /> Only show products in stock
+        <input
+          type="checkbox"
+          checked={checkboxValue}
+          onChange={(event) => onCheckboxChange(event.target.checked)}
+        />{' '}
+        Only show products in stock
       </label>
     </form>
   )
